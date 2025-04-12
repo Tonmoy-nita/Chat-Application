35427c4618b4969b18536ecfc28b30a33c4e1f32
TONMOY <tonmoybk198@gmail.com>
2025-04-13 01:14:46 +0530

    `Added uploader middleware and updated singleUploader utility to handle file uploads`

--- middlewares/users/avatarUpload.js
+++ middlewares/users/avatarUpload.js
@@ -1,3 +1,5 @@
+const uploader = require("../../utilities/singleUploader.js");
+
 function avatarUpload(req, res, next) {
   const upload = uploader(
     "avatar",
@@ -5,5 +7,21 @@ function avatarUpload(req, res, next) {
     1000000,
     "Only .jpg,.jpeg or .png format allowed"
   );
+
+  //call the middleware function
+
+  upload.any()(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        errors: {
+          avatar: {
+            msg: err.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
 }
 module.exports = avatarUpload;
--- utilities/singleUploader.js
+++ utilities/singleUploader.js
@@ -1,3 +1,7 @@
+//external imports
+const multer = require("multer");
+const path = require("path");
+
 function uploader(
   subfolder_path,
   allowed_file_types,
@@ -6,6 +10,42 @@ function uploader(
 ) {
   //file upload folder(jodi file upload e kono somosha hoi then ekhane ase check korte hobe)
   const UPLOADS_FOLDER = `${__dirname}/../../public/uploads/${subfolder_path}`;
+
+  //define the storage
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+      const fileExt = path.extname(file.originalname);
+      const fileName =
+        file.originalname
+          .replace(fileExt, "")
+          .toLowerCase()
+          .split(" ")
+          .join("-") +
+        "-" +
+        Date.now();
+
+      cb(null, fileName + fileExt);
+    },
+  });
+
+  //prepare the final multer object
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: max_file_size,
+    },
+    fileFilter: (req, res, cb) => {
+      if (allowed_file_types.include(file.minetype)) {
+        cb(null, true);
+      } else {
+        cb(createError(error_msg));
+      }
+    },
+  });
+
   return uploader;
 }
 

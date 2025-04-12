75a0bd2ece56977fb0c16b55439b280818aa9fd5
TONMOY <tonmoybk198@gmail.com>
2025-04-13 01:17:23 +0530

    `Added avatar upload middleware and updated users route to handle POST requests`

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,11 +4,15 @@ const express = require("express");
 //internal impports
 const { getUsers } = require("../controller/usersController.js");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse.js");
+const avatarUpload = require("../middlewares/users/avatarUpload.js");
 
 const router = express.Router();
 
 //Users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+//add user
+router.post("/", avatarUpload);
 
 //export routes
 module.exports = router;

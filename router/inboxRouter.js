3e26da43c2c5cfa2991b289b921f74d701527adf
TONMOY <tonmoybk198@gmail.com>
2025-04-18 03:06:34 +0530

    `Added checkLogin middleware to inboxRouter.js and usersRouter.js`

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -4,11 +4,12 @@ const express = require("express");
 //internal impports
 const { getInbox } = require("../controller/inboxController.js");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse.js");
+const checkLogin = require("../middlewares/common/checkLogin.js");
 
 const router = express.Router();
 
 //Inbox page
-router.get("/", decorateHtmlResponse("Inbox"), getInbox);
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 //export routes
 module.exports = router;
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -7,21 +7,26 @@ const {
   addUser,
   removeUser,
 } = require("../controller/usersController.js");
+
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse.js");
 const avatarUpload = require("../middlewares/users/avatarUpload.js");
+
 const {
   addUserValidators,
   addUserValidationHandler,
 } = require("../middlewares/users/userValidators.js");
 
+const checkLogin = require("../middlewares/common/checkLogin.js");
+
 const router = express.Router();
 
 //Users page
-router.get("/", decorateHtmlResponse("Users"), getUsers);
+router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
 
 //add user
 router.post(
   "/",
+  checkLogin,
   avatarUpload,
   addUserValidators,
   addUserValidationHandler,

ff099bde273680c947a0cd36e6e6d40acbd0578d
TONMOY <tonmoybk198@gmail.com>
2025-04-19 02:36:06 +0530

    `Update error handling middleware: fix not found handler message and simplify error rendering logic`

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -1,31 +1,24 @@
 const createError = require("http-errors");
-const { title } = require("process");
 
-//404 not found handler
+// 404 not found handler
 function notFoundHandler(req, res, next) {
-  next(createError(404, "Your requested content was not found"));
+  next(createError(404, "Your requested content was not found!"));
 }
 
-//default error handler
+// default error handler
 function errorHandler(err, req, res, next) {
-  //   res.render("error", {
-  //     title: "Error page",
-  //   });
-  //upor r error message send ta ke amra r ak rokom vabe korte pari
-  //   res.locals.title = "Error page";
-  //   res.render("error");
-
   res.locals.error =
     process.env.NODE_ENV === "development" ? err : { message: err.message };
 
   res.status(err.status || 500);
 
   if (res.locals.html) {
+    // html response
     res.render("error", {
       title: "Error page",
-      error: res.locals.error,
     });
   } else {
+    // json response
     res.json(res.locals.error);
   }
 }
